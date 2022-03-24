c9b76f7ad7845412e201678b5661902f3b1066e4
jcontent <jcontent@jcontent.lyon.tennaxia.org>
2022-03-24 09:53:15 +0100

    Correction exiftool test

--- spec/exif/mini_exiftool_manipulateur_spec.rb
+++ spec/exif/mini_exiftool_manipulateur_spec.rb
@@ -11,18 +11,19 @@ RSpec.describe MiniExiftoolManipulateur do
       @dossier_tmp = FileUtils.makedirs "#{FileHelpers::TMP}test01"
     end
 
-    where(:case_name, :nom_fichier, :date_time_original) do
+    where(:case_name, :nom_fichier, :date_time_original, :type, :extension) do
       [
-        ["Date time original au '2020:02:01 01:01:01'", "photo_2020_02_01-01-01-01", DateTime.new(2020, 2, 1, 1, 1, 1)]
+        ["Date time original au '2020:02:01 01:01:01'", "photo_2020_02_01-01-01-01", DateTime.new(2020, 2, 1, 1, 1, 1, '+1'), FileHelpers::IMAGE, ".jpeg"],
+        ["Date time original au '2020:02:01 01:01:01'", "video_2020_02_01-01-01-01", DateTime.new(2020, 2, 1, 1, 1, 1), FileHelpers::VIDEO, ".mp4"]
       ]
     end
     with_them do
       it do
-        image = ImageHelpers.creer_("#{FileHelpers::TMP}test01", nom_fichier, FileHelpers::IMAGE)
+        fichier = ImageHelpers.creer_("#{FileHelpers::TMP}test01", nom_fichier, type, extension)
         
-        MiniExiftoolManipulateur.new.set_datetimeoriginal(image, date_time_original)
+        MiniExiftoolManipulateur.new.set_datetimeoriginal(fichier, date_time_original)
 
-        expect(ExifHelpers.get_datetime(image)).to be date_time_original
+        expect(ExifHelpers.get_datetime(fichier)).to eql date_time_original
       end
 
       after do
@@ -56,19 +57,5 @@ RSpec.describe MiniExiftoolManipulateur do
         FileUtils.rm_rf(@dossier_tmp[0])
       end
     end
-  end
-
-  before do
-    @dossier_tmp = FileUtils.makedirs "#{FileHelpers::TMP}test05"
-  end
-  it do
-    video = ImageHelpers.creer_("#{FileHelpers::TMP}test05", "video_2020_02_01-01-01-01", FileHelpers::VIDEO, "mp4")
-
-    MiniExiftoolManipulateur.new.set_datetimeoriginal(video, DateTime.new(2020, 2, 1, 1, 1, 1))
-
-    expect(ExifHelpers.get_datetime(video)).to be DateTime.new(2020, 2, 1, 1, 1, 1)
-  end
-  after do
-    FileUtils.rm_rf(@dossier_tmp[0])
   end
 end
--- spec/helpers/exif_helpers.rb
+++ spec/helpers/exif_helpers.rb
@@ -5,6 +5,9 @@ require "mini_exiftool"
 # Exif helpers
 module ExifHelpers
   def self.get_datetime(fichier)
-    MiniExiftool.new(fichier).date_time_original
+    date_time_original = MiniExiftool.new(fichier).date_time_original
+    unless MiniExiftool.new(fichier).date_time_original.nil?
+      DateTime.parse(date_time_original.to_s)
+    end
   end
 end
